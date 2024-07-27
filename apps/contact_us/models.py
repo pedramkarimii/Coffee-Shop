ae0766357565fddd76376b7f2b0e6bdde5f9f408
pedramkarimii <pedram@9060.com>
2024-07-27 05:03:57 +0330

    :zap: Add models: ContactMessage

---
+++ apps/contact_us/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class ContactMessage(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    subject = models.CharField(max_length=200)
+    message = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)
+
+    def __str__(self):
+        return self.subject
