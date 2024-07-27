00ad58bfa1acb20a91ae58649b63b3f938ddecab
pedramkarimii <pedram@9060.com>
2024-07-27 05:05:54 +0330

    :zap: Add URL patterns and admin interface customization for debug mode

---
+++ config/urls.py
@@ -0,0 +1,29 @@
+from django.conf import settings
+from django.contrib import admin
+from django.urls import path, include
+
+"""Define URL patterns for the entire application."""
+urlpatterns = [
+    path("", include("apps.accounts.urls")),
+    path("menu/", include("apps.menu.urls")),
+    path("cart/", include("apps.orders.urls")),
+    path("", include("apps.contact_us.urls")),
+    path("", include("apps.reservetable.urls")),
+    path('', include('apps.core.urls')),
+]
+
+"""Check if the application is in debug mode."""
+if settings.DEBUG:
+    from django.conf.urls.static import static
+
+    """Add URL pattern for accessing the Django admin site."""
+    urlpatterns += (
+            [path("admin/", admin.site.urls)]
+            + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+            + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    )
+
+    """Customize Django admin interface titles."""
+    admin.site.site_header = 'Online Shop'
+    admin.site.site_title = 'Online Shop Administration'
+    admin.site.index_title = 'Welcome To Online Shop Administration'
